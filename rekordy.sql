f586a27cd25de228863976cebd4c68e2ad0d0e8e
karol2432 <109307364+karol2432@users.noreply.github.com>
2023-11-15 00:28:23 +0100

    Update rekordy.sql

--- rekordy.sql
+++ rekordy.sql
@@ -1,3 +1,16 @@
+INSERT INTO rozliczenia.pracownicy (id_pracownika, imie, nazwisko, adres, telefon)
+VALUES
+    ('1', 'Karol', 'Nowak', 'Warszawa', '555555555'),
+    ('2', 'Piotr', 'Karbarz', 'Kraków', '123456789'),
+    ('3', 'Joanna', 'Wsiór', 'Łódź', '987654321'),
+    ('4', 'Wojciech', 'Glut', 'Kraków', '123459876'),
+    ('5', 'Maciej', 'Duda', 'Sosnowiec', '987612345'),
+    ('6', 'Weronika', 'Lewandowski', 'Poznań', '119876543'),
+    ('7', 'Dominika', 'Kowalski', 'Poznań', '112233445'),
+    ('8', 'Błażej', 'Kowalski', 'Warszawa', '192837465'),
+    ('9', 'Kacper', 'But', 'Kraków', '657438291'),
+    ('10', 'Ola', 'Nowak', 'Sosnowiec', '234567891');
+
 INSERT INTO rozliczenia.godziny (id_godziny, data_, liczba_godzin, id_pracownika)
 VALUES
     ('1', '2023.10.25', '8', '1'),
@@ -10,7 +23,20 @@ VALUES
     ('8', '2023.11.01', '7', '8'),
     ('9', '2023.11.02', '6', '9'),
     ('10', '2023.11.03', '8', '10');
-	
+
+INSERT INTO rozliczenia.premie (id_premii, rodzaj, kwota)
+VALUES
+    ('11', 'nr 1', '500'),
+    ('12', 'nr 2', '400'),
+    ('13', 'nr 3', '300'),
+    ('14', 'nr 4', '450'),
+    ('15', 'nr 5', '420'),
+    ('16', 'nr 6', '510'),
+    ('17', 'nr 7', '410'),
+    ('18', 'nr 8', '320'),
+    ('19', 'nr 9', '460'),
+    ('20', 'nr 10', '430');	
+
 INSERT INTO rozliczenia.pensje (id_pensji, stanowisko, kwota, id_premii)
 VALUES
     ('1', 'Prezes', '10000', '11'),
@@ -24,28 +50,5 @@ VALUES
     ('9', 'Ksiegowy', '4600', '19'),
     ('10', 'Programista', '4300', '20');
 	
-INSERT INTO rozliczenia.pracownicy (id_pracownika, imie, nazwisko, adres, telefon)
-VALUES
-    ('1', 'Karol', 'Nowak', 'Warszawa', '555555555'),
-    ('2', 'Piotr', 'Karbarz', 'Kraków', '123456789'),
-    ('3', 'Joanna', 'Wsiór', 'Łódź', '987654321'),
-    ('4', 'Wojciech', 'Glut', 'Kraków', '123459876'),
-    ('5', 'Maciej', 'Duda', 'Sosnowiec', '987612345'),
-    ('6', 'Weronika', 'Lewandowski', 'Poznań', '119876543'),
-    ('7', 'Dominika', 'Kowalski', 'Poznań', '112233445'),
-    ('8', 'Błażej', 'Kowalski', 'Warszawa', '192837465'),
-    ('9', 'Kacper', 'But', 'Kraków', '657438291'),
-    ('10', 'Ola', 'Nowak', 'Sosnowiec', '234567891');
 	
-INSERT INTO rozliczenia.premie (id_premii, rodzaj, kwota)
-VALUES
-    ('11', 'nr 1', '500'),
-    ('12', 'nr 2', '400'),
-    ('13', 'nr 3', '300'),
-    ('14', 'nr 4', '450'),
-    ('15', 'nr 5', '420'),
-    ('16', 'nr 6', '510'),
-    ('17', 'nr 7', '410'),
-    ('18', 'nr 8', '320'),
-    ('19', 'nr 9', '460'),
-    ('20', 'nr 10', '430');
+
