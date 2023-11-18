0dfe6de6b6524faffd0d6c2894d954a07853d547
karol2432 <109307364+karol2432@users.noreply.github.com>
2023-11-18 21:17:06 +0100

    Delete fk.zaj4.sql

--- fk.zaj4.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE rozliczenia.godziny
-ADD FOREIGN KEY (id_pracownika)
-REFERENCES rozliczenia.pracownicy(id_pracownika)
-ON DELETE SET NULL;
-
-ALTER TABLE rozliczenia.pensje
-ADD FOREIGN KEY (id_premii)
-REFERENCES rozliczenia.premie(id_premii)
-ON DELETE SET NULL;
